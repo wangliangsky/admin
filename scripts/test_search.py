2838343ab915abca437c1fed9da1f257f1b3b620
wangliangsky <1163131801@qq.com>
2018-05-27 16:19:43 +0800

    test03

--- scripts/test_search.py
+++ scripts/test_search.py
@@ -1,10 +1,15 @@
 import allure, pytest
 class Test_allure:
     def setup(self):
-         print("132")
+         print("1231")
     def teardown(self):
          pass
     @pytest.mark.parametrize("a",[1,2,3])
     @allure.step('我是测试步骤001')
     def test_al(self, a):
+        assert a != 2
+
+    @allure.step('我是测试步骤002')
+    @pytest.mark.parametrize("a", [1, 5, 6])
+    def test_a2(self, a):
         assert a != 2
